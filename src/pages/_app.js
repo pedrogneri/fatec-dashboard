90afe098381edeaef78a2308569bd4aaeca6dd18
Pedro Neri <pedrogneri@gmail.com>
2020-09-22 13:45:15 -0300

    Add font on next/head on _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,12 @@
 import { GlobalStyle } from 'styles.js';
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }) => {
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans" />
+      </Head>
       <GlobalStyle />
       <Component {...pageProps} />
     </>
--- src/styles.js
+++ src/styles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-
   * {
     margin: 0;
     padding: 0;
