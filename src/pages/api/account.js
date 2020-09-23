0c270d9f7d3792a98c98f41d18bf0e5de1ad125f
Pedro Neri <pedrogneri@gmail.com>
2020-09-23 14:02:53 -0300

    Try to fix prod issue

--- src/pages/api/account.js
+++ src/pages/api/account.js
@@ -4,7 +4,7 @@ export default async function (req, res) {
   if (req.method === 'POST') {
     const {user, password} = req.body;
     login(user, password);
-    sleep(2000);
+    await sleep(2000);
 
     res.statusCode = 200
     res.setHeader('Content-Type', 'application/json')
@@ -17,8 +17,8 @@ export default async function (req, res) {
 }
 
 export const getAccount = async () => {
-  const name = await myAccount.getName();
-  const schoolGrade = await myAccount.getSchoolGrade();
+  const name = await myAccount?.getName();
+  const schoolGrade = await myAccount?.getSchoolGrade();
   const disciplines = JSON.parse(JSON.stringify(schoolGrade.semesters[0].disciplines));
  
   return { name, disciplines, error: null };
