56bd5c45d5e86f7ccf9db0dcf5d0271f2c82cf08
Pedro Neri <pedrogneri@gmail.com>
2020-09-23 20:31:01 -0300

    Prod test

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import { Account } from 'fatec-api';
-export let myAccount = { name: 'Pedro' };
+export let myAccount;
 
 export const login = (login, password) => {
   myAccount = new Account(login, password);
